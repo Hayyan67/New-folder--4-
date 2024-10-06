6f59d1fc355a977dc4857eda66854b755bc83b34
Hayyan67 <humairabaig81@gmail.com>
2024-10-06 14:57:39 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#empty list
+
+l1=[]
+print('Empty list ',l1)
+#List with elements
+l2=[56,23,78,98,12,6,87]
+print('list with elements ',l2)
+
+#list with mixed datatype
+l3=[12,2,3,True,'abc']
+print('List with mixed datatypes: ',l3)
+
+#Repeat
+print('Repeating: ',13*3)
+
+
+#Reverse
+print('Reversing: ',12[::-1])
